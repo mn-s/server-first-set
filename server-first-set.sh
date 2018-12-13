fcbc980f28bf6f5de2c87b306e1919af5f5cd5b7
mengniu8 <34342026+mengniu8@users.noreply.github.com>
2018-12-13 21:29:20 +0800

    Create server-first-set.sh

---
+++ server-first-set.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+read -p "input add username:" username
+read -p "input password of ${username}:" userpass
+read -p "input root password(press enter to skip):" rootpass
+read -p "input ssh port:" port
+echo -e "\033[36muser ${username}'s password is:${userpass}\033[0m"
+echo -e "\033[36myour root password:${rootpass}\033[0m"
+echo -e "\033[36myour ssh port is:${port}\033[0m"
+read -p "is continue?[Y/n]" isok
+if [ "$isok" == 'n' ]; then
+        echo -e "\033[31mended\033[0m"
+        exit 1
+fi
+
+yum -y update
+yum install -y screen zip unzip sendmail lrzsz
+
+if cat /etc/passwd | grep ^${username}; then
+        echo "user ${username} already exist."
+else
+        echo "add user:${username}"
+        useradd ${username}
+fi
+echo ${username}:${userpass} | chpasswd
+if [ "${rootpass}" != '' ]; then
+        echo "root:${rootpass}" | chpasswd
+else
+        echo -e "\033[31mDo not set root pass.\033[0m"
+fi
+sed -in "s/[# ]*Port [0-9]\+/Port ${port}/" /etc/ssh/ssh_config
+sed -in "s/[# ]*Port [0-9]\+/Port ${port}/" /etc/ssh/sshd_config
+sed -in 's/[# ]*PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
+if iptables -L -n | grep ${port}; then
+        echo "port ${port} is already opened."
+else
+        echo "add iptables rule for port:${port}"
+        iptables -A INPUT -p tcp --dport ${port} -j ACCEPT
+fi
+iptables-save
+echo "close selinux."
+setenforce 0
+sed -n '/SELINUX/s/enforcing/disabled/' /etc/selinux/config
+service sshd restart
+echo "your ssh port is:${port}"
+echo -e "\033[31mPlease remove old port rule from iptables by yourself.The command is:\n\033[36miptables -D -p tcp --dport {yourport} -j ACCEPT\033[0m"
